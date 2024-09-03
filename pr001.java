53cf5eb6ef950743037cab54869db63a4375086f
dmitriybairamov <bairamovvdm@gmail.ru>
2024-09-03 14:11:47 +0700

    dobavleni peremennie and primeri deleniya

--- pr001.java
+++ pr001.java
@@ -1,6 +1,12 @@
 class pr001 {
 	public static void main(String args[]) {
-		System.out.println("Pervaya programma na Java");
-		
-	}
+		System.out.println("Pervaya programma na Java"); // comment stroka na console
+		int Var1 = 1024;
+		int Var2345678;
+		Var2345678 = 411;
+		System.out.println(Var1/2);
+		System.out.println("result: " + Var2345678/2);
+		System.out.println("result privedeniem tipa double: " + (double)Var2345678/2);
+		double Var3 = (double)Var2345678/2;
+		}
 }
