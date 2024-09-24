9d7d7645185178efcd7fb1ce55c5628031ec51f8
dmitriybairamov <bairamovvdm@gmail.ru>
2024-09-24 13:07:30 +0700

    variants ispolzovaniya cycle

---
+++ pr003.java
@@ -0,0 +1,42 @@
+class pr003 {
+	public static void main(String args[]) throws java.io.IOException {
+
+		// chtenie s klavatyri
+		char ch;
+		System.out.print("Nazhmite klavishy na klaviatyre and ENTER");
+		ch = (char) System.in.read();
+		System.out.println("Vi nazhali klavishy: " + ch);
+
+		// operatorator cycle for
+
+		System.out.println ("\n");
+		double num, sroot, rerr;
+
+		for (num=1.0; num < 100.0; num++) {
+			sroot = Math.sqrt(num);
+			System.out.println ("Kvadratnii koren chisli " + num + " raven " + sroot);
+			// vichslenie oshibki okryglenia
+			rerr = num - (sroot * sroot);
+			System.out.println ("Oshibkaokryglenia sostavliaet " + rerr);
+			System.out.println();
+		}
+
+		int x1;
+
+		for (x1 = 100; x1 > -100; x1 -= 5)
+			System.out.println (x1);
+
+		System.out.println("\n");
+
+		int i7, j7;
+		for (i7 = 0, j7 = 10; i7 < j7; i7++, j7--)
+			System.out.println("i7 and j7: " + i7 + " " + j7);
+		
+		System.out.println ("\n");
+
+		int i8;
+		System.out.println ("Dlya ostanovki cycle najmite klavishy \"S\"");
+		for (i8 = 0; (char) System.in.read() != 'S'; i8++)
+			System.out.println("Iteration №" + i8);
+		}			
+}
