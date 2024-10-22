5013d9370e03ba7c8debed6c8f67dbcc15e60689
dmitriybairamov <bairamovvdm@gmail.ru>
2024-10-22 14:13:48 +0700

    Rabota nad strokami

--- pr005.java
+++ pr005.java
@@ -40,6 +40,141 @@ class Arrays {
                 }
                 System.out.println ("Minimal and max znachenie: " + min + " " + max);		
 
+		//Пузырьковая сортировка
+                System.out.println();
+                int size = sample2.length;
+                int a, b, t;
+                //Отображение исходного массива
+                System.out.println("Исходный массив: ");
+                for(i=0; i < size; i++)
+                        System.out.print(" " + sample2[i]);
+                System.out.println();
+
+                //Собственно сортировка
+                for(a=1; a < size; a++)
+                        for(b=size-1; b >= a; b--) {
+                                if(sample2[b-1]>sample2[b]) {
+                                        t = sample2[b-1];
+                                        sample2[b-1] = sample2[b];
+                                        sample2[b] = t;
+                                }
+                        }
+                //отображение от сортирового массива
+                System.out.println("Отсортированный массив:");
+                for(i=0; i < size; i++)
+                        System.out.print(" " + sample2[i]);
+                System.out.println();
+
+                //Двумерные массивы
+                System.out.println();
+                int [][] arr2D = new int [10][20];
+                int t2, i2;
+                for(t2=0; t2<10; t2++) {
+for(i2=10; i2<30; i2++) {
+                                arr2D[t2][i2-10] = (t2*10)+i2+1;
+                                if (t2==0)
+                                        System.out.print(arr2D[t2][i2-10] + "  ");
+                                else
+                                        System.out.print(arr2D[t2][i2-10] + " ");
+
+                        }
+                        System.out.println();
+                }
+
+                //Двумерные массивы с разным кол-во элементов в строке
+                System.out.println();
+
+                int [][] riders = new int[7][];
+                riders[0] = new int[10];
+                riders[1] = new int[10];
+                riders[2] = new int[10];
+                riders[3] = new int[10];
+                riders[4] = new int[10];
+                riders[5] = new int[2];
+                riders[6] = new int[2];
+
+                for(int i3=0; i3<7; i3++)
+                        for(int j3=0; j3<riders[i3].length; j3++)
+                                riders[i3][j3] = i3 + j3 + 10;
+
+                System.out.println("Количество пассажиров в рабочие дни недели: ");
+                for(int i3=0; i3<5; i3++) {
+                        for(int j3=0; j3<riders[i3].length; j3++)
+                                System.out.print(riders[i3][j3] + " ");
+                        System.out.println();
+                }
+
+                System.out.println();
+                System.out.println("Количество пассажиров в выходные дни недели: ");
+for(int i3=5; i3<7; i3++) {
+                        for(int j3=0; j3<2; j3++)
+                                System.out.print(riders[i3][j3] + " ");
+                        System.out.println();
+                }
+
+                //Ссылки на массивы
+                System.out.println();
+                for (int i3=0; i3<10; i3++)
+                        sample[i3] = i3;
+                for (int i3=0; i3<10; i3++)
+                        sample1[i3] = -i3;
+
+                System.out.println("Массивы sample[]: ");
+                for (int i3=0; i3<10; i3++)
+                        System.out.print(sample[i3] + " ");
+                System.out.println();
+                System.out.println("Масиивы sample1[]: ");
+                for (int i3=0; i3<10; i3++)
+                        System.out.print(sample1[i3] + " ");
+                System.out.println();
+                sample1 = sample;
+                sample1[4] = 1111;
+                System.out.println("Массивы sample[] после изменения элемента с индексом 4 в sample1[]: ");
+                for (int i3=0; i3<10; i3++)
+                        System.out.print(sample[i3] + " ");
+                System.out.println();
+                System.out.println("sample[]: " + sample + "sample1[]: " + sample1);
+
+                //Копирование массива
+                System.out.println();
+                int[] sample3 = new int[10];
+                for (int i3=0; i3<10; i3++)
+                        sample3[i3] = -i3;
+                if(sample1.length >= sample3.length)
+                        for( int i3=0; i3<sample3.length; i3++)
+				sample1[i3] = sample3[i3];
+
+                //Вывод содержимого sample1 после копирования
+                System.out.println("Массивы sample1[] полсе копирования: ");
+                for (int i3=0; i3<sample1.length; i3++)
+                        System.out.print(sample1[i3] + " ");
+                System.out.println();
+
+
+		// cycle "for-each"
+		System.out.println ();
+		int summ = 0;
+		for (int i3 = 0; i3 < 10; i3++)
+			summ += sample [i3];
+		System.out.println(summ);
+
+		summ = 0;
+		for (int x: sample3) {
+			System.out.println("Ocheerednoe znachenie: " + x);
+			summ += x;
+		}
+		System.out.println(summ);
+
+		summ = 0;
+                for(int[] x: riders) {
+			for(int y : x) {
+                        	System.out.println("Znachenie: " + y);
+                        	summ += y;
+			}
+			System.out.println("New stroka massiva: ");
+                }
+                System.out.println("Summa: " + summ);
+		
 
 
 	}
