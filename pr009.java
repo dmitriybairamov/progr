73305ab6efdce70f08aabbc15f9e504a03513c70
dmitriybairamov <bairamovvdm@gmail.ru>
2024-11-19 12:52:22 +0700

    peregryzka konstryktorov v superclasse and podklassax

--- pr009.java
+++ pr009.java
@@ -3,6 +3,20 @@
 class TwoDShape {
 	private double width;
 	private double height;
+	//konstryktor bez parametrov
+	TwoDShape() {
+		width = height = 0.0;
+	}
+	//konstruktor s 2 parametrami
+	TwoDShape(double w, double h) {
+		width = w;
+		height = h;
+	}
+	//konstryktor object s odinak w and h
+	TwoDShape(double x) {
+		width = height = x;
+	}
+
 	void showDim() {
 		System.out.println("Shirina and visota" + width + " and " + height);
 	}
@@ -27,15 +41,25 @@ class TwoDShape {
 
 class Triangle extends TwoDShape {
 	String style;
+	
+	//konstryktor po umolcaniyou
+	Triangle() {
+		super();
+		style = "otsytstvyet";
+	}
 	//konstruktor
 	Triangle(String s, double w, double h) {
+		// vizov konstyktora super classa
+		super(w, h);
 		//ystanovka znachenia dlya peremennoi podklassa
 		style = s;
-		//ystanovka znach dlya peremennix syperklassa
-		setWidth(w);
-		setHeight(h);
 	}
-	
+	//kostryktor s 1 parametrom
+	Triangle(double x) {
+		super(x);
+		style = "zakrashenniy";
+	}
+
 	double area() {
 		return getWidth()*getHeight()/2;
 	}
@@ -48,13 +72,21 @@ class Triangle extends TwoDShape {
 class Rectangle extends TwoDShape {
 	String outline;
 	
-	//konstrutor
+	//konstrutori
+	Rectangle() {
+		super();
+		outline = "otsytstvyet";
+	}
+
+	Rectangle (double x) {
+		super(x);
+		outline = "sploshnaya";
+	}
 	Rectangle(String o, double w, double h) {
+		// vizov konstryktora super classa
+		super(w, h);
 		//ystanovka znachenia dlya peremennoi podklassa
 		outline=o;
-		//ystanovka znachenia dlya peremennix syperklassa
-		setWidth(w);
-		setHeight(h);
 	}
 
 	double area() {
@@ -72,10 +104,11 @@ class Rectangle extends TwoDShape {
 
 class pr009 {
 	public static void main(String[] args) {
-		Triangle t1 = new Triangle("zakrashenniy", 4.0, 4.0);
+		Triangle t1 = new Triangle();
 		Triangle t2 = new Triangle("konturniy", 8.0, 12.0);
+		Triangle t3 = new Triangle(4.0);
 		Rectangle r1 = new Rectangle("sploshnaya", 4.0, 4.0);
-		Rectangle r2 = new Rectangle("pynktirnaya", 8.0, 12.0);
+		Rectangle r2 = new Rectangle(5.0);
 /*		
 		t1.setWidth (4.0);
 		t1.setHeight (4.0);
@@ -90,16 +123,26 @@ class pr009 {
                 r2.setHeight (12.0);
                 r2.outline = "punktirnaya";
 */
+		t1 = t2;
+
 		System.out.println("Info about t1: ");
 		t1.showStyle();
 		t1.showDim();
 		System.out.println("S: " + t1.area());
 		System.out.println();
+
 		System.out.println("Info about t2: ");
 		t2.showStyle();
         	t2.showDim();
-        	System.out.println("S: " + t2.area());
+		System.out.println("S: " + t2.area());
+                System.out.println();
+
+		System.out.println("Info about t3: ");
+                t3.showStyle();
+                t3.showDim();
+        	System.out.println("S: " + t3.area());
         	System.out.println();
+
 		System.out.println("Info about r1: ");
                 r1.showOutline();
                 r1.showDim();
